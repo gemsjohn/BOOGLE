c3738b867b28ea193e3ea672515f792f197c8175
JRM <johnrhmartin@gmail.com>
2022-05-21 17:21:32 -0400

    updated login and addUser mutations to accept token

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,21 +1,13 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User } = require("../models");
-const bookSchema = require('../models/Book');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
       me: async (parent, {username}) => {
         const params = username ? { username } : {}
-        // return User.find().sort({ createdAt: -1 });
         return User.findOne({ username })
       }
-      // me: async (parent, { _id }) => {
-      //   return User.findOne({ _id });
-      // }
-      // books: async (parent, {bookId}) => {
-      //   const params = bookId ? { bookId } : {}
-      //   return Book.find().sort({ createdAt: -1 });
-      // }
     },
     Mutation: {
       login: async (parent, { email, password }) => {
@@ -31,11 +23,13 @@ const resolvers = {
           throw new AuthenticationError('Incorrect credentials');
         }
 
-        return user;
+        const token = signToken(user);
+        return { token, user };
       },
       addUser: async (parent, args) => {
         const user = await User.create(args);
-        return user;
+        const token = signToken(user);
+        return { token, user };
       }
     }
   };
