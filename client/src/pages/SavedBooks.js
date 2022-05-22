9e074e88e683efac4e12e4d17dadd9940d0e285b
JRM <johnrhmartin@gmail.com>
2022-05-22 15:39:40 -0400

    updated SavedBooks to useQuery(GET_ME)

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,44 +1,20 @@
-import { useQuery } from '@apollo/client';
-import { GET_ME } from '../utils/queries';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
+import { useQuery } from '@apollo/client';
+import { GET_ME, GET_ME_BASIC, GET_BOOKS } from '../utils/queries';
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import { useParams } from 'react-router-dom';
 
 const SavedBooks = () => {
-  const [ setUserData] = useState({});
-
-  // use useQuery hook to make query request
   const { userData } = useQuery(GET_ME);
+  const user = userData?.me || [];
 
-  const userDataLength = Object.keys(userData).length;
-
-  useQuery(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
+  if (!user.length) {
+    return <h2>LOADING...</h2>;
+  }
+  console.log(user);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -55,19 +31,14 @@ const SavedBooks = () => {
         throw new Error('something went wrong!');
       }
 
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
+      // const updatedUser = await response.json();
+      // setUserData(updatedUser);
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
-
-  // if data isn't here yet, say so
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
-  }
 
   return (
     <>
@@ -79,7 +50,7 @@ const SavedBooks = () => {
       <Container>
         <h2>
           {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+            ? `Viewing ${userData.me.savedBooks.length} saved ${userData.me.savedBooks.length === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
         <CardColumns>
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,11 +1,30 @@
 import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
-    query Me {
+    {
         me {
             _id
             username
             email
+            bookCounter
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
         }
     }
+`;
+
+export const GET_ME_BASIC = gql`
+  {
+    me {
+      _id
+      username
+      email
+    }
+  }
 `;
